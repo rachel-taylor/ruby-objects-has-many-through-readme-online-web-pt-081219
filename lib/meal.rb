4712e993e72f82a63af5bae97f2add93c3bd608b
Rachel T <rachmaybe@gmail.com>
2019-10-24 05:31:42 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -16,11 +16,11 @@ class Meal
     @@all
   end
   
-  sam = Customer.new("Sam", 27)
-  pat = Waiter.new("Pat", 2)
-  alex = Waiter.new("Alex", 5)
+  # sam = Customer.new("Sam", 27)
+  # pat = Waiter.new("Pat", 2)
+  # alex = Waiter.new("Alex", 5)
   
-  sam.new_meal(pat, 50, 10) # A Customer creates a Meal, passing in a Waiter instance
-  sam.new_meal(alex, 20, 3) # A Customer creates a Meal, passing in a Waiter instance
-  pat.new_meal(sam, 30, 5) # A Waiter creates a Meal, passing in a Customer instance
+  # sam.new_meal(pat, 50, 10) # A Customer creates a Meal, passing in a Waiter instance
+  # sam.new_meal(alex, 20, 3) # A Customer creates a Meal, passing in a Waiter instance
+  # pat.new_meal(sam, 30, 5) # A Waiter creates a Meal, passing in a Customer instance
 end
